378f8c7bb3eb47ad14a045092a3d7097f0d2841a
nthmost <naomi@nthmost.com>
2026-05-27 15:36:30 -0700

    Export trait helper functions in __init__.py

--- stellaris_save_parser/__init__.py
+++ stellaris_save_parser/__init__.py
@@ -6,7 +6,17 @@ A Python library for parsing and analyzing Stellaris save files.
 
 from .save import StellarisSave
 from .models import Empire, Planet, Leader
-from .traits import TraitCategory, categorize_trait
+from .traits import TraitCategory, categorize_trait, get_trait_description, is_governor_production_trait, get_optimal_planet_type
 
 __version__ = "0.1.0"
-__all__ = ["StellarisSave", "Empire", "Planet", "Leader", "TraitCategory", "categorize_trait"]
+__all__ = [
+    "StellarisSave",
+    "Empire",
+    "Planet",
+    "Leader",
+    "TraitCategory",
+    "categorize_trait",
+    "get_trait_description",
+    "is_governor_production_trait",
+    "get_optimal_planet_type"
+]
